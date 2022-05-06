def5a67a5477d47df1b1104b00643ed504e8a252
pxie2016 <xie.zim@northeastern.edu>
2022-05-06 16:52:20 -0700

    Revamp deployment structure

--- server.js
+++ server.js
@@ -24,10 +24,10 @@ app.use('/users', userroutes);
 
 let port = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(port, () => {
